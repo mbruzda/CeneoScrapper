4fc0dfefe768d8f3e1bbb27832354c23b5fbd7a4
mbruzda <marcin.bru@op.pl>
2020-04-15 13:36:16 +0200

    MB 15.04.2020

--- scrapper.py
+++ scrapper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import pprint
 import json
 
-#funkcja do ekstrakcji składników opinii
+#funkcja do ekstrakcji składowych opinii
 def extract_feature(opinion, selector, attribute = None):
     try:
         if not attribute:
@@ -14,36 +14,35 @@ def extract_feature(opinion, selector, attribute = None):
     except IndexError:
         return None
 
-
-#lista składowych opini wraz z selektorami i atrybutami
+#lista składowych opinii wraz z selektorami i atrybutami
 selectors = {
-    "author":['div.reviewer-name-line'],
-    "recomendation":['div.product-review-summary > em'],
+    "author": ['div.reviewer-name-line'],
+    "recommendation":['div.product-review-summary > em'],
     "stars":['span.review-score-count'],
     "content": ['p.product-review-body'],
     "pros": ['div.pros-cell > ul'],
-    "cons":['div.cons-cell > ul'],
-    "useful":['button.vote-yes',"data-total-vote"],
-    "useless":['button.vote-no',"data-total-vote"],
+    "cons":['div.cons-cell > ul'], 
+    "useful":['button.vote-yes', "data-total-vote"],
+    "useless":['button.vote-no', "data-total-vote"],
     "purchased":['div.product-review-pz'],
-    "purchase_date":['span.review-time > time:nth-child(2)',"datetime"],
-    "review_date":['span.review-time > time:nth-child(1)',"datetime"]
+    "purchase_date":['span.review-time > time:nth-of-type(1)',"datetime"],
+    "review_date":['span.review-time > time:nth-of-type(2)',"datetime"]
 }
 
-#funkcjia do usuwania znaków formatujących
+#funkcja do usuwania znaków formatujących
 def remove_whitespaces(text):
-    for char in ["\n", "\r"]:
-        try:
-            return text.replace(char, ". ")
-        except AttributeError:
-            pass
+    try:
+        for char in ["\n", "\r"]:
+            text = text.replace(char, ". ")
+        return text
+    except AttributeError:
+        pass
 
 #adres URL strony z opiniami
 url_prefix = "https://www.ceneo.pl"
-product_id = "76891706"#input("Podaj kod produktu: ")
+product_id = input("Podaj kod produktu: ")
 url_postfix = "#tab=reviews"
 url = url_prefix+"/"+product_id+url_postfix
-
 
 #pusta lista na opinie
 opinions_list = []
@@ -58,17 +57,17 @@ while url is not None:
     
     #ekstrakcja składowyh dla pojedynczej opinii z listy
     for opinion in opinions: 
-        opinion_id = opinion["data-entry-id"]
-
+      
         features = {key:extract_feature(opinion, *args)
                     for key, args in selectors.items()}
         features["opinion_id"] = int(opinion["data-entry-id"])
         features["purchased"] = True if features["purchased"] == "Opinia potwierdzona zakupem" else False
         features["useful"] = int(features["useful"])
         features["useless"] = int(features["useless"])
-        features["content"]=remove_whitespaces(features["content"])
-        features["pros"]=remove_whitespaces(features["pros"])
-        features["cons"]=remove_whitespaces(features["cons"])
+        features["content"] = remove_whitespaces(features["content"])
+        features["pros"] = remove_whitespaces(features["pros"])
+        features["cons"] = remove_whitespaces(features["cons"])
+        
 
         opinions_list.append(features)
 
@@ -79,5 +78,10 @@ while url is not None:
 
     print("url:",url)
 
-with open(", 'w', encoding='utf-8') as fp:
-    json.dump(opinions_list, fp, ensure_ascii=False, indent=4)
+with open("opinions/"+product_id+".json", 'w', encoding="UTF-8") as fp:
+    json.dump(opinions_list, fp, ensure_ascii=False, separators=(",",": "), indent=4 )
+
+
+# print(len(opinions_list))
+# for opinion in opinions_list:
+#     pprint.pprint(opinion)
