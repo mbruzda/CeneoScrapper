16a19ae6d81f96dd653e103b6834ae9255f57150
mbruzda <marcin.bru@op.pl>
2020-05-06 11:38:34 +0200

    MB 2020.06.05

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,15 @@
+  
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Regexp
+from wtforms.validators import DataRequired,Length,Regexp
 
-class ProductFrom(FlaskForm):
-    product_code = StringField("Podaj kod produktu do pobrania opinii", 
-    validators=[
-        DataRequired(message='Musisz podac kod produktu'),
-        Length(min = 8, max = 8, message="Kod produktu musi miec 8 znaków"),
-        Regexp(regex = "[0-9]*", message="Kod produkutu musi moze zawierac tylko cyfry")
+class ProductForm(FlaskForm):
+    product_code = StringField(
+        "Podaj kod produktu do pobrania opinii", 
+        validators=[
+            DataRequired(message="Musisz podać kod produktu"),
+            Length(min=8, max=8, message="Kod produktu musi mieć 8 znaków"),
+            Regexp(regex="[0-9]{8}", message = "Kod produktu może zawierać tylko cyfry")
         ]
     )
     submit = SubmitField("Pobierz")
-
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,32 @@
 from app import app
 from flask import render_template
 from flaskext.markdown import Markdown
+from app.forms import ProductForm
+app.config['SECRET_KEY'] = "Tajemniczy_mysi_sprzęt"
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
+@app.route("/about")
+def about():
+    content = ""
+    with open("README.md", "r" ,encoding="utf-8") as f:
+        content = f.read()
+    return render_template("about.html", text = content)
+
+@app.route("/extract")
 def extract():
+    # if request.method == "POST":
+    #     return "Przesłano formularz"
+    form = ProductForm()
+    return render_template("extract.html", form = form)
+
+@app.route("/products")
+def products():
     return "Podaj kod produktu do pobrania opinii"
 
-@app.route('/products')
-def productsf():
-    pass
-
-@app.route('/about')
-def about():
-    content = ''
-    with open("README.md", 'r', encoding="UTF-8") as f:
-        content = f.read() 
-    return render_template("about.html", tekst=content)
-
-@app.route('/analyzer/<product_id>')
+@app.route("/analyzer/<product_id>")
 def analyzer():
     return "Podaj kod produktu do analizy"
